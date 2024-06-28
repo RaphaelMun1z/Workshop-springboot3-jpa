77a8a8a4b94195df2df951fc542ef0ca698799ad
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-06-27 22:43:23 -0300

    feat: Exception handling - update

--- src/main/java/com/aula/course/services/UserService.java
+++ src/main/java/com/aula/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.aula.course.repositories.UserRepository;
 import com.aula.course.services.exceptions.DatabaseException;
 import com.aula.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -47,9 +49,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateData(User entity, User obj) {
